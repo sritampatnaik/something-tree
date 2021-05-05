d9dfa853a3273cbf8efed667e6db7b705d817735
Sritam Patnaik <sritampatnaik@gmail.com>
2021-05-05 22:13:23 +0800

    Styling changes

--- app/src/components/loggedIn/Billing.vue
+++ app/src/components/loggedIn/Billing.vue
@@ -2,7 +2,7 @@
 	<div class="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
 		<!-- Manage Subs -->
 		<section v-if="claims" aria-labelledby="plan_heading_no_subs">
-			<div class="shadow sm:rounded-md sm:overflow-hidden">
+			<div class="sm:overflow-hidden">
 				<div class="bg-white py-6 px-4 space-y-6 sm:p-6">
 					<div>
 						<h2 id="plan_heading" class="text-lg leading-6 font-medium text-gray-900">Plan</h2>
@@ -42,7 +42,35 @@
 						</div>
 					</div>
 				</div>
-				<div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+				<div class="px-4 py-3 border-gray-50 text-right sm:px-6">
+					<button
+						v-if="!isProductsLoading"
+						@click="createCustomerPotal('isCancelSubsClick')"
+						type="button"
+						:disabled="paymentState"
+						class="ml-2 text-gray-600 border border-transparent rounded-md py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 disabled:opacity-50"
+					>
+						<svg
+							v-if="isCancelSubsClick"
+							class="animate-spin h-5 w-5 mr-2 -ml-1"
+							viewBox="0 0 40 40"
+							enable-background="new 0 0 40 40"
+						>
+							<path
+								fill="#fff"
+								d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+    s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+    c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"
+							/>
+							<path
+								fill="#DC2626"
+								opacity="1"
+								d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+    C22.32,8.481,24.301,9.057,26.013,10.047z"
+							/>
+						</svg>
+						{{ isCancelSubsClick? "Loading" : "Cancel subscription" }}
+					</button>
 					<button
 						v-if="!isProductsLoading"
 						@click="createCustomerPotal('isManageSubsClick')"
@@ -69,35 +97,7 @@
     C22.32,8.481,24.301,9.057,26.013,10.047z"
 							/>
 						</svg>
-						{{ isManageSubsClick? "Loading" : "Manage subscription" }}
-					</button>
-					<button
-						v-if="!isProductsLoading"
-						@click="createCustomerPotal('isCancelSubsClick')"
-						type="button"
-						:disabled="paymentState"
-						class="ml-2 bg-red-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 disabled:opacity-50"
-					>
-						<svg
-							v-if="isCancelSubsClick"
-							class="animate-spin h-5 w-5 mr-2 -ml-1"
-							viewBox="0 0 40 40"
-							enable-background="new 0 0 40 40"
-						>
-							<path
-								fill="#fff"
-								d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
-    s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
-    c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"
-							/>
-							<path
-								fill="#DC2626"
-								opacity="1"
-								d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
-    C22.32,8.481,24.301,9.057,26.013,10.047z"
-							/>
-						</svg>
-						{{ isCancelSubsClick? "Loading" : "Cancel subscription" }}
+						{{ isManageSubsClick? "Loading" : "Manage Subscriptions" }}
 					</button>
 				</div>
 			</div>
@@ -211,68 +211,68 @@
 
 		<!-- Billing history -->
 		<section aria-labelledby="billing_history_heading">
-			<div class="bg-white pt-6 shadow sm:rounded-md sm:overflow-hidden">
+			<div class="bg-white pt-6 sm:overflow-hidden">
 				<div class="px-4 sm:px-6">
 					<h2
 						id="billing_history_heading"
 						class="text-lg leading-6 font-medium text-gray-900"
 					>Billing history</h2>
-				</div>
-				<div v-if="isBillingHistroyLoading" class="w-full border border-gray-300 rounded-md p-4">
-					<div class="animate-pulse flex justify-between">
-						<span class="h-4 bg-indigo-400 rounded w-1/6"></span>
-						<span class="h-4 -ml-2 bg-indigo-400 rounded w-1/6"></span>
-						<span class="h-4 -ml-10 bg-indigo-400 rounded w-20"></span>
-						<span class="h-4 bg-indigo-400 rounded w-1/6"></span>
+					<div v-if="isBillingHistroyLoading" class="w-full border border-gray-300 rounded-md p-4">
+						<div class="animate-pulse flex justify-between">
+							<span class="h-4 bg-indigo-400 rounded w-1/6"></span>
+							<span class="h-4 -ml-2 bg-indigo-400 rounded w-1/6"></span>
+							<span class="h-4 -ml-10 bg-indigo-400 rounded w-20"></span>
+							<span class="h-4 bg-indigo-400 rounded w-1/6"></span>
+						</div>
 					</div>
-				</div>
-				<div v-else class="mt-6 flex flex-col">
-					<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-						<div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-							<div class="overflow-hidden border-t border-gray-200">
-								<table class="min-w-full divide-y divide-gray-200">
-									<thead class="bg-gray-50">
-										<tr>
-											<th
-												scope="col"
-												class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-											>Date</th>
-											<th
-												scope="col"
-												class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-											>Invoice Number</th>
-											<th
-												scope="col"
-												class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-											>Amount</th>
-											<!--
-                            `relative` is added here due to a weird bug in Safari that causes `sr-only` headings to introduce overflow on the body on mobile.
-											-->
-											<th
-												scope="col"
-												class="relative px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-											>
-												<span class="sr-only">View receipt</span>
-											</th>
-										</tr>
-									</thead>
-									<tbody class="bg-white divide-y divide-gray-200">
-										<tr v-for="(payment, key) in billingHistory" :key="key">
-											<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-												<time>{{ payment.created.toDateString() }}</time>
-											</td>
-											<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ payment.invoice_id }}</td>
-											<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ payment.amount }}</td>
-											<td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-												<a
-													:href="payment.invoiceURL"
-													target="_blank"
-													class="text-indigo-600 hover:text-indigo-900 cursor-pointer"
-												>View Invoice</a>
-											</td>
-										</tr>
-									</tbody>
-								</table>
+					<div v-else class="my-6 flex flex-col">
+						<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+							<div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+								<div class="overflow-hidden rounded-md border border-gray-200">
+									<table class="min-w-full divide-y divide-gray-200">
+										<thead class="bg-gray-50">
+											<tr>
+												<th
+													scope="col"
+													class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>Date</th>
+												<th
+													scope="col"
+													class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>Invoice Number</th>
+												<th
+													scope="col"
+													class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>Amount</th>
+												<!--
+								`relative` is added here due to a weird bug in Safari that causes `sr-only` headings to introduce overflow on the body on mobile.
+												-->
+												<th
+													scope="col"
+													class="relative px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>
+													<span class="sr-only">View receipt</span>
+												</th>
+											</tr>
+										</thead>
+										<tbody class="bg-white divide-y divide-gray-200">
+											<tr v-for="(payment, key) in billingHistory" :key="key">
+												<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+													<time>{{ payment.created.toDateString() }}</time>
+												</td>
+												<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ payment.invoice_id }}</td>
+												<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ payment.amount }}</td>
+												<td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+													<a
+														:href="payment.invoiceURL"
+														target="_blank"
+														class="text-indigo-600 hover:text-indigo-900 cursor-pointer"
+													>View Invoice</a>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
 							</div>
 						</div>
 					</div>

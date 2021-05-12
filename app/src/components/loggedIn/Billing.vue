522c0f0445329937efaaa5529ffc30ff3b2b8c52
Ayush <ayushkgupta1998@gmail.com>
2021-05-12 13:25:20 +0530

    Removed comments from billing

--- app/src/components/loggedIn/Billing.vue
+++ app/src/components/loggedIn/Billing.vue
@@ -283,37 +283,6 @@
 </template>
 
 <script>
-	// import { ref } from "vue";
-	// const plans = [
-	// 	{
-	// 		name: "Startup",
-	// 		priceMonthly: 29,
-	// 		priceYearly: 290,
-	// 		limit: "Up to 5 active job postings",
-	// 	},
-	// 	{
-	// 		name: "Business",
-	// 		priceMonthly: 99,
-	// 		priceYearly: 990,
-	// 		limit: "Up to 25 active job postings",
-	// 	},
-	// 	{
-	// 		name: "Enterprise",
-	// 		priceMonthly: 249,
-	// 		priceYearly: 2490,
-	// 		limit: "Unlimited active job postings",
-	// 	},
-	// ];
-	// const payments = [
-	// 	{
-	// 		id: 1,
-	// 		date: "1/1/2020",
-	// 		datetime: "2020-01-01",
-	// 		description: "Business Plan - Annual Billing",
-	// 		amount: "CA$109.00",
-	// 		href: "#",
-	// 	},
-	// ];
 	import {
 		RadioGroup,
 		RadioGroupDescription,
@@ -333,19 +302,6 @@
 			SwitchGroup,
 			SwitchLabel,
 		},
-		// setup() {
-		// 	const open = ref(false);
-		// 	const selectedPlan = ref(plans[1]);
-		// 	const annualBillingEnabled = ref(true);
-
-		// 	return {
-		// 		plans,
-		// 		payments,
-		// 		open,
-		// 		selectedPlan,
-		// 		annualBillingEnabled,
-		// 	};
-		// },
 		data() {
 			return {
 				open: false,

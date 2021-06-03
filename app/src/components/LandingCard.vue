7a728b25e3d7de6a5b4a4c62da263bcecf1818fd
Sritam Patnaik <sritampatnaik@gmail.com>
2021-06-03 22:14:37 +0800

    Different page

--- app/src/components/LandingCard.vue
+++ app/src/components/LandingCard.vue
@@ -15,9 +15,9 @@
         </p>
         <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
             <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-1 sm:gap-5">
-            <a href="#" @click="startQuiz" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8">
+            <button @click="startQuiz" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8">
                 Start Quiz
-            </a>
+            </button>
             </div>
         </div>
         </div>
@@ -27,8 +27,10 @@
 <script>
 export default {
     methods : {
-        startQuiz () {
-            this.$store.dispatch("quiz/startQuiz")
+        async startQuiz () {
+            await this.$store.dispatch("quiz/startQuiz")
+            this.$router.push({ name: 'Quiz' })
+
         }
     }
 }
